f88b388ad5cfacc1eb62a7eb1a3a2f17cfeb7319
Costin <costin@me.com>
2025-06-18 15:01:39 -0400

    Add code implementation

--- src/Order.java
+++ src/Order.java
@@ -1,22 +1,78 @@
-public class Order {
-    public void attach(OrderObserver orderObserver) {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
+public class Order {
+    private List<OrderObserver> observers;
+
+    private int orderNumber;
+    private List<Double> orderItems;
+    private double totalPrice;
+    private int orderQuantity;
+    private int shippingCost;
+
+    public Order() {
+        observers = new ArrayList<OrderObserver>();
+
+        Random rand = new Random();
+        this.orderNumber = rand.nextInt(1000);
+        this.orderItems = new ArrayList<>();
+        this.totalPrice = 0;
+        this.orderQuantity = 0;
+        this.shippingCost = 10;
+    }
+
+    public void attach(OrderObserver orderObserver) {
+        this.observers.add(orderObserver);
     }
 
     public void detach(OrderObserver orderObserver) {
+        this.observers.remove(orderObserver);
+    }
 
+    public void addItem(double price) {
+        this.orderItems.add(price);
+        this.totalPrice = orderItems.stream().mapToDouble(Double::doubleValue).sum();
+        this.orderQuantity = orderItems.size();
+        this.notifyObservers();
+    }
+
+    public void notifyObservers() {
+        for (OrderObserver observer : observers) {
+            observer.update(this);
+        }
+    }
+
+    public List<Double> getOrderItems() {
+        return orderItems;
+    }
+
+    public int getOrderQuantity() {
+        return orderQuantity;
+    }
+
+    public void setOrderQuantity(int orderQuantity) {
+        this.orderQuantity = orderQuantity;
     }
 
     public double getTotalPrice() {
-        return 0.0;
+        return this.totalPrice;
     }
 
-    public int getCount() {
-        return 0;
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public int getShippingCost() {
+        return shippingCost;
+    }
+
+    public void setShippingCost(int shippingCost) {
+        this.shippingCost = shippingCost;
     }
 
     @Override
     public String toString() {
-        return null;
+        return "Order number: " + this.orderNumber + ", containing " + this.orderQuantity + " item(s) for a total of $" + this.totalPrice + " (+ $" + this.shippingCost + " for shipping)";
     }
 }
--- src/QuantityObserver.java
+++ src/QuantityObserver.java
@@ -1,2 +1,7 @@
-public class QuantityObserver {
+public class QuantityObserver implements OrderObserver {
+    @Override
+    public void update(Order order) {
+        if (order.getOrderQuantity() > 5)
+            order.setShippingCost(0);
+    }
 }
