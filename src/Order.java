5e7f691532613b1f9cd0e21874fb2c16685d54c1
Costin <costin@me.com>
2025-06-18 14:04:09 -0400

    Add predefined classes signatures

--- src/Order.java
+++ src/Order.java
@@ -1,3 +1,22 @@
 public class Order {
+    public void attach(OrderObserver orderObserver) {
 
+    }
+
+    public void detach(OrderObserver orderObserver) {
+
+    }
+
+    public double getTotalPrice() {
+        return 0.0;
+    }
+
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public String toString() {
+        return null;
+    }
 }
